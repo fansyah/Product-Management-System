820f506c2412e618f37f50287c72ab067c1a16c1
Rifki Fauzi <kubido@gmail.com>
2015-05-25 22:31:16 +0700

    add sorting

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,14 +5,20 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-        
-        @keyword = params[:filter][:name] if params[:filter].present?
-        if @keyword.present?
-          @products = Product.paginate(:page => params[:page], :per_page => 5).where("name LIKE ?", "%#{@keyword}%")
-        else
-          @products = Product.paginate(:page => params[:page], :per_page => 5)
-        end
+    # 'desc' atau descending = diurutkan dari yg akhir ke awal. Z - A / paling lama dulu baru ke yg baru
+    #  ascending atau ascending = diurutkan dari yg awal ke akhir. A - Z / paling baru dulu baru ke yg lama
+    products = Product.order('name desc') if params[:urutkan] == "nama" 
+    products = Product.order('created_at desc') if params[:urutkan] == "tanggal"
 
+
+    @keyword = params[:filter][:name] if params[:filter].present?
+    if @keyword.present?
+      @products = products.paginate(:page => params[:page], :per_page => 5).where("name LIKE ?", "%#{@keyword}%")
+    else
+      @products = products.paginate(:page => params[:page], :per_page => 5)
+    end
+
+    
   end
   # GET /products/1
   # GET /products/1.json
