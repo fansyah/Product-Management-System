1756c94f09fb14cdeeaeabccb9aaa74795248ab3
Rifki Fauzi <kubido@gmail.com>
2015-06-20 13:27:57 +0700

    remove debugger, display image in home page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,7 +46,6 @@ class ProductsController < ApplicationController
   # POST /products
   # POST /products.json
   def create
-    debugger
     @product = Product.new(product_params)
 
     respond_to do |format|
